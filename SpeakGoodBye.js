838a17926bc445d84ab344818320971b7cfc8c95
teopap <teo-27@hotmail.com>
2016-12-24 20:41:43 +0200

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,8 +1,27 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-	<key>URL</key>
-	<string>https://github.com/jhu-ep-coursera/fullstack-course4/blob/master/assignments/assignment4/assignment4-solution-starter/easier/SpeakGoodBye.js</string>
-</dict>
-</plist>
+var byeSpeaker = {};
+// NOTE! The steps in this file are basically identical to the ones you
+// performed in the SpeakHello.js file.
+
+// STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
+// See Lecture 52, part 2
+(function () {
+    // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
+    // the "speak" method and which you will expose to the global context
+    // See Lecture 52, part 1
+    //var byeSpeaker
+
+    // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
+    var speakWord = "Good Bye";
+
+    // STEP 8: Rewrite the 'speak' function such that it is attached to the
+    // byeSpeaker object instead of being a standalone function.
+    // See Lecture 52, part 2
+    byeSpeaker.speak = function (name) {
+        console.log(speakWord + " " + name);
+    }
+
+    // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
+    // 'byeSpeaker' on the global scope as well.
+    // xxxx.xxxx = byeSpeaker;
+
+})();
